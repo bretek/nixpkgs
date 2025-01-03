451f8ccdf3798a82abbd43d1f4ad872f237508ee
bretek <josephmadden999@protonmail.com>
2025-01-03 19:28:37 +0000

    rzls: init at 9.0.0-preview.24569.4

---
+++ pkgs/by-name/rz/rzls/package.nix
@@ -0,0 +1,99 @@
+{ lib
+, fetchFromGitHub
+, buildDotnetModule
+, dotnetCorePackages
+, stdenvNoCC
+, jq
+,
+}:
+let
+  pname = "rzls";
+  dotnet-sdk =
+    with dotnetCorePackages;
+    sdk_9_0
+    // {
+      inherit
+        (combinePackages [
+          sdk_9_0
+          sdk_8_0
+        ])
+        packages
+        targetPackages
+        ;
+    };
+  # need sdk on runtime as well
+  dotnet-runtime = dotnetCorePackages.sdk_9_0;
+  rid = dotnetCorePackages.systemToDotnetRid stdenvNoCC.targetPlatform.system;
+
+in
+buildDotnetModule {
+  inherit pname dotnet-sdk dotnet-runtime;
+
+  src = fetchFromGitHub {
+    owner = "dotnet";
+    repo = "razor";
+    rev = "7390745dcd9c8831d4459437ed2e9e94125f3dd3";
+    hash = "sha256-czzeIZ7m/vEQFu2PYIVu7duKgzodHGXG6czV56kr43Q=";
+  };
+
+  # versioned independently from vscode-csharp
+  # "razor" in here:
+  # https://github.com/dotnet/vscode-csharp/blob/main/package.json
+  version = "9.0.0-preview.24569.4";
+  projectFile = "src/Razor/src/rzls/rzls.csproj";
+  useDotnetFromEnv = true;
+  nugetDeps = ./deps.json;
+
+  nativeBuildInputs = [ jq ];
+
+  postPatch = ''
+    # Upstream uses rollForward = latestPatch, which pins to an *exact* .NET SDK version.
+    jq '.sdk.rollForward = "latestMinor"' < global.json > global.json.tmp
+    mv global.json.tmp global.json
+  '';
+
+  dotnetFlags = [
+    "-p:TargetRid=${rid}"
+    # this removes the Microsoft.WindowsDesktop.App.Ref dependency
+    "-p:EnableWindowsTargeting=false"
+  ];
+
+  # two problems solved here:
+  # 1. --no-build removed -> BuildHost project within roslyn is running Build target during publish
+  # 2. missing crossgen2 7.* in local nuget directory when PublishReadyToRun=true
+  # the latter should be fixable here but unsure how
+  installPhase = ''
+    runHook preInstall
+
+    env dotnet publish $dotnetProjectFiles \
+        -p:ContinuousIntegrationBuild=true \
+        -p:Deterministic=true \
+        -p:InformationalVersion=$version \
+        -p:UseAppHost=true \
+        -p:PublishTrimmed=false \
+        -p:PublishReadyToRun=false \
+        --configuration Release \
+        --no-self-contained \
+        --output "$out/lib/$pname" \
+        --no-restore \
+        --runtime ${rid} \
+        ''${dotnetInstallFlags[@]}  \
+        ''${dotnetFlags[@]}
+
+    runHook postInstall
+  '';
+
+  #passthru = {
+  #tests.version = testers.testVersion { package = roslyn-ls; };
+  #updateScript = ./update.sh;
+  #};
+
+  meta = {
+    homepage = "https://github.com/dotnet/vscode-csharp";
+    description = "Language server behind C# Dev Kit for Visual Studio Code";
+    changelog = "https://github.com/dotnet/vscode-csharp/releases/tag/v";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ konradmalik ];
+    mainProgram = "Microsoft.Razor.CodeAnalysis.LanguageServer";
+  };
+}
