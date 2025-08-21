ea8664aa3d033f0c5b6e78c1f3fffa383502c42d
bretek <josephmadden999@gmail.com>
2025-08-21 14:00:42 +0100

    xow_dongle-firmware: linked firmware with different name
    Dongle was searching for firmware with different name, linked that name
    to base firmware.

--- pkgs/by-name/xo/xow_dongle-firmware/package.nix
+++ pkgs/by-name/xo/xow_dongle-firmware/package.nix
@@ -40,6 +40,7 @@ stdenvNoCC.mkDerivation rec {
 
   installPhase = ''
     install -Dm644 xow_dongle.bin $out/lib/firmware/xow_dongle.bin
+    ln $out/lib/firmware/xow_dongle.bin $out/lib/firmware/xow_dongle_045e_02fe.bin
     install -Dm644 xow_dongle_045e_02e6.bin $out/lib/firmware/xow_dongle_045e_02e6.bin
   '';
 
